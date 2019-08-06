177a1addd4a98408e802785da342b22167c5605a
byNoobiYT <47305525+byNoobiYT@users.noreply.github.com>
2019-08-06 16:57:32 +0200

    Activity command (Added todo)

---
+++ src/main/java/io/github/bynoobiyt/nightdream/commands/Activity.java
@@ -0,0 +1,36 @@
+package io.github.bynoobiyt.nightdream.commands;
+
+import io.github.bynoobiyt.nightdream.util.BotData;
+import io.github.bynoobiyt.nightdream.util.Utils;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import static net.dv8tion.jda.api.entities.Activity.playing;
+
+// TODO: 06.08.2019 Write new game into NightDream.properties 
+@BotCommand("activity")
+public class Activity implements Command {
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+        if (args.length == 0) {
+            event.getChannel().sendMessageFormat("%s, Something went very wrong! <:IconX:553868311960748044>", event.getMember().getAsMention()).queue();
+            return;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (String arg : args) {
+            builder.append(arg).append(" ");
+        }
+        event.getJDA().getPresence().setActivity(playing(BotData.getDefaultPrefix() + " | " + builder.toString()));
+
+        event.getChannel().sendMessage("Done: " + BotData.getDefaultPrefix() + " | " + builder.toString()).queue();
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+
+    @Override
+    public boolean allowExecute(String[] args, MessageReceivedEvent event) {
+        return Utils.checkOwner(event);
+    }
+}
