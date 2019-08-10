159cb75791acc4e8b6c08102699964ac558fc092
byNoobiYT <47305525+byNoobiYT@users.noreply.github.com>
2019-08-10 21:01:48 +0200

    Import cleanup

--- src/main/java/io/github/bynoobiyt/nightdream/commands/BotCommand.java
+++ src/main/java/io/github/bynoobiyt/nightdream/commands/BotCommand.java
@@ -1,9 +1,6 @@
 package io.github.bynoobiyt.nightdream.commands;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * Annotation for a Command
