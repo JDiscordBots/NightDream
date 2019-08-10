3873555e8ae3fc98006b19947a25d5a165645fb7
danthe1st <daniel@wwwmaster.at>
2019-08-10 15:32:19 +0200

    improved/rewrite Calc

--- src/main/java/io/github/bynoobiyt/nightdream/commands/Calc.java
+++ src/main/java/io/github/bynoobiyt/nightdream/commands/Calc.java
@@ -1,86 +1,55 @@
 package io.github.bynoobiyt.nightdream.commands;
 
 import io.github.bynoobiyt.nightdream.util.BotData;
-import io.github.bynoobiyt.nightdream.util.Utils;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.awt.Color;
 
-// TODO: 08.08.2019 TEST
 @BotCommand("calc")
 public class Calc implements Command {
 
-	private static String operation;
-	private static float result;
-	private static TextChannel channel;
-	private static float num1;
-	private static float num2;
 
 	@Override
 	public void action(String[] args, MessageReceivedEvent event) {
-		channel = event.getTextChannel();
+		String operation="";
 		String wrongFormat = String.format("<:IconProvide:553870022125027329> Format: `%scalc num1 [+,-,*,/] num2`", BotData.getDefaultPrefix());
-		if (args.length == 0) {
-			event.getChannel().sendMessage(wrongFormat).queue();
-			return;
-		}
-		String[] ops = {"+", "-", "*", "/"};
-
-		List<String> operations = new ArrayList<>(Arrays.asList(ops));
-
-		if (!operations.contains(args[1]) || args[2] == null) {
+		if (args.length < 2) {
 			event.getChannel().sendMessage(wrongFormat).queue();
 			return;
 		}
 		try {
-			Integer.parseInt(args[0]);
-			Integer.parseInt(args[2]);
-	        } catch (Exception e) {
-		        event.getChannel().sendMessage(wrongFormat).queue();
-                        return;
-		}
-		String operator = args[1];
-
-		num1 = Float.parseFloat(args[0]);
-		num2 = Float.parseFloat(args[2]);
-
-		switch (operator) {
-			case "+":
-				result = num1 + num2;
-				operation = "plus";
-				make(result, operation);
-				break;
-			case "-":
-				result = num1 - num2;
-				operation = "minus";
-				make(result, operation);
-				break;
-			case "*":
-				result = num1 * num2;
-				operation = "by";
-				make(result, operation);
-				break;
-			case "/":
-				result = num1 / num2;
-				operation = "divided by";
-				make(result, operation);
-				break;
-			default:
-				Utils.errmsg(event.getTextChannel(), "Failed with error.");
-				break;
-		}
-	}
-
-	private void make(float result, String operation) {
-		EmbedBuilder eb = new EmbedBuilder().setColor(Color.white).setTitle(String.format("%s %s %s", num1, operation, num2))
-				.setDescription(String.valueOf(result));
-
-		channel.sendMessage(eb.build()).queue();
+			double num1 = Float.parseFloat(args[0]);
+			double num2 = Float.parseFloat(args[2]);
+			String operator = args[1];
+			double result;
+			switch (operator) {
+				case "+":
+					result = num1 + num2;
+					operation = "plus";
+					break;
+				case "-":
+					result = num1 - num2;
+					operation = "minus";
+					break;
+				case "*":
+					result = num1 * num2;
+					operation = "by";
+					break;
+				case "/":
+					result = num1 / num2;
+					operation = "divided by";
+					break;
+				default:
+					event.getChannel().sendMessage(wrongFormat).queue();
+					return;
+			}
+			EmbedBuilder eb = new EmbedBuilder().setColor(Color.white).setTitle(String.format("%s %s %s", num1, operation, num2))
+					.setDescription(String.valueOf(result));
+			event.getTextChannel().sendMessage(eb.build()).queue();
+        } catch (NumberFormatException e) {
+	        event.getChannel().sendMessage(wrongFormat).queue();
+        }
 	}
 
 	@Override
