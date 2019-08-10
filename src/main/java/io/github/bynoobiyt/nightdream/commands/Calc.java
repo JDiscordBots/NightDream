93ca27bf2b34f2c3f0c7191445c8550c22bb793d
byNoobiYT <47305525+byNoobiYT@users.noreply.github.com>
2019-08-10 21:08:11 +0200

    Update Calc.java

--- src/main/java/io/github/bynoobiyt/nightdream/commands/Calc.java
+++ src/main/java/io/github/bynoobiyt/nightdream/commands/Calc.java
@@ -1,47 +1,39 @@
 package io.github.bynoobiyt.nightdream.commands;
 
 import io.github.bynoobiyt.nightdream.util.BotData;
-import io.github.bynoobiyt.nightdream.util.Utils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// TODO: 08.08.2019 TEST
 @BotCommand("calc")
 public class Calc implements Command {
 
-	private static String operation;
-	private static float result;
-	private static TextChannel channel;
-	private static float num1;
-	private static float num2;
+	private String operation;
+	private float result;
+	private TextChannel channel;
+	private float num1;
+	private float num2;
 
 	@Override
 	public void action(String[] args, MessageReceivedEvent event) {
+		List<String> operations = Arrays.asList("+", "-", "*", "/");
+
 		channel = event.getTextChannel();
 		String wrongFormat = String.format("<:IconProvide:553870022125027329> Format: `%scalc num1 [+,-,*,/] num2`", BotData.getDefaultPrefix());
-			if (args.length == 0) {
-				event.getChannel().sendMessage(wrongFormat).queue();
-				return;
-			}
-			String[] ops = {"+", "-", "*", "/"};
-
-			List<String> operations = new ArrayList<>(Arrays.asList(ops));
-
-			if (!operations.contains(args[1]) || args[2] == null) {
-				event.getChannel().sendMessage(wrongFormat).queue();
-				return;
-			}
-			try {
-				Integer.parseInt(args[0]);
-				Integer.parseInt(args[2]);
+		if (args.length == 0 || !operations.contains(args[1]) || args[2] == null) {
+			event.getChannel().sendMessage(wrongFormat).queue();
+			return;
+		}
+		try {
+			Integer.parseInt(args[0]);
+			Integer.parseInt(args[2]);
 		} catch (Exception e) {
 			event.getChannel().sendMessage(wrongFormat).queue();
+			return;
 		}
 		String operator = args[1];
 
@@ -70,7 +62,7 @@ public class Calc implements Command {
 				make(result, operation);
 				break;
 			default:
-				Utils.errmsg(event.getTextChannel(), "Failed with error.");
+				event.getChannel().sendMessage(wrongFormat).queue();
 				break;
 		}
 	}
