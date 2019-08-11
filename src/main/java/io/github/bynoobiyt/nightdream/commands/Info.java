591752c32aca2cf3822f034b9d21bf99b1ca1ec0
danthe1st <daniel@wwwmaster.at>
2019-08-11 15:31:27 +0200

    instance is owned by not hard-coded

--- src/main/java/io/github/bynoobiyt/nightdream/commands/Info.java
+++ src/main/java/io/github/bynoobiyt/nightdream/commands/Info.java
@@ -1,6 +1,8 @@
 package io.github.bynoobiyt.nightdream.commands;
 
 import io.github.bynoobiyt.nightdream.core.NightDream;
+import io.github.bynoobiyt.nightdream.util.BotData;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -8,6 +10,8 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @BotCommand("info")
 public class Info implements Command {
@@ -30,9 +34,11 @@ public class Info implements Command {
 
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
+    	final JDA jda=event.getJDA();
         if (event.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_EXT_EMOJI)) {
+        	
             event.getChannel().sendMessageFormat("In %s guilds, serving %s users and %s bots.\r\n" +
-                    "This instance is owned by dan1st#7327 and Gehasstes#0001.\r\n" +
+                    "This instance is owned by "+Stream.of(BotData.getAdminIDs()).map((id)->jda.getUserById(id).getAsTag()).collect(Collectors.joining(" and "))+".\r\n" +
                     "JDA v4.BETA.0_32\r\n" +
                     "Logo Font: Avenir Next LT Pro / (c) Linotype\r\n" +
                     "(c) dan1st and Gehasstes %s, Release %s.", event.getJDA().getGuilds().size(), getUsers(), getBots(), Year.now().getValue(), NightDream.VERSION).queue();
