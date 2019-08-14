2f42bfcd3c29fb6acd4a1c6be17d6fa42cc55d48
danthe1st <daniel@wwwmaster.at>
2019-08-14 11:26:16 +0200

    Javadoc for BotData and GeneralUtils

--- src/main/java/io/github/bynoobiyt/nightdream/util/BotData.java
+++ src/main/java/io/github/bynoobiyt/nightdream/util/BotData.java
@@ -13,6 +13,10 @@ import java.util.Properties;
 import io.github.bynoobiyt.nightdream.commands.Profile;
 import net.dv8tion.jda.api.entities.Guild;
 
+/**
+ * saving, loading, retrieving and setting data of the Bot
+ * @author Daniel Schmid
+ */
 public class BotData {
 	private static Properties defaultProps;
 	private static final Map<Guild,Properties> guildProps = new HashMap<>();
@@ -30,9 +34,17 @@ public class BotData {
 		//prevent Instantiation
 	}
 	
+	/**
+	 * gets the Prefix for all guilds with no specified prefix
+	 * @return the prefix
+	 */
 	public static String getDefaultPrefix() {
 		return getDefaultProperties().getProperty(PREFIX_PROP_NAME,"nd-");
 	}
+	/**
+	 * sets the Prefix for all guilds with no specified prefix
+	 * @param prefix the prefix
+	 */
 	public static void setDefaultPrefix(String prefix) {
 		if (prefix == null || prefix.equals("")) {
 			prefix = "nd-";
@@ -41,73 +53,165 @@ public class BotData {
 		props.setProperty(PREFIX_PROP_NAME, prefix);
 		saveGuildDefaultProperties(props);
 	}
+	/**
+	 * gets the prefix of a specified Guild or the default prefix
+	 * @param g the {@link Guild}
+	 * @return the prefix
+	 */
 	public static final String getPrefix(Guild g) {
 		return getProperty(PREFIX_PROP_NAME, g);
 	}
+	/**
+	 * sets the prefix of a specified Guild or the default prefix
+	 * @param g the {@link Guild}
+	 * @param prefix the prefix
+	 */
 	public static void setPrefix(Guild g,String prefix) {
 		setProperty(PREFIX_PROP_NAME, prefix, g);
 	}
+	/**
+	 * sets the channel for message (delete) logs
+	 * @param channelId the ISnowflake id of the channel
+	 * @param guild the {@link Guild} where the prefix should be set
+	 */
 	public static void setMsgLogChannel(String channelId, Guild guild) {
 		setProperty("MsgLogChannel", channelId, guild);
 	}
+	/**
+	 * gets the channel for message (delete) logs
+	 * @param guild the {@link Guild} where the prefix should be set
+	 * @return the ISnowflake id of the channel
+	 */
 	public static String getMsgLogChannel(Guild guild) {
 		return getProperty("MsgLogChannel", guild);
 	}
+	/**
+	 * resets/unsets the channel for message (delete) logs
+	 * @param guild the {@link Guild} where the prefix should be reset
+	 */
 	public static void resetMsgLogChannel(Guild guild) {
 		setProperty("MsgLogChannel", "", guild);
 	}
+	/**
+	 * resets the prefix for a {@link Guild} (sets it to the default prefix for all Guilds)
+	 * @param g the {@link Guild} where the prefix should be reset
+	 */
 	public static void resetPrefix(Guild g) {
 		setPrefix(g, getDefaultPrefix());
 	}
+	/**
+	 * gets the instance owners of the bot
+	 * @return the instance owners as array of ISnowflake IDs
+	 */
 	public static String[] getAdminIDs() {
 		return getGlobalProperty("admin").split(" ");
 	}
+	/**
+	 * sets the instance owners of the bot
+	 * @param adminIDs the instance owners as array of ISnowflake IDs
+	 */
 	public static void setAdminIDs(String[] adminIDs) {
 		setGlobalProperty("admin", String.join(" ",adminIDs));
 	}
+	/**
+	 * gets a Property for a {@link Guild}
+	 * @param key the key(name) of the Property
+	 * @param g the {@link Guild}
+	 * @return the value of the Property
+	 */
 	private static String getProperty(String key, Guild g) {
 		return getGuildSpecificProperties(g).getProperty(key);
 	}
+	/**
+	 * sets a Property for a {@link Guild}
+	 * @param key the key(name) of the Property
+	 * @param value the value of the Property
+	 * @param g the {@link Guild}
+	 */
 	private static void setProperty(String key, String value, Guild g) {
 		Properties props = getGuildSpecificProperties(g);
 		props.setProperty(key, value);
 		saveGuildSpecificProperties(props, g);
 	}
+	/**
+	 * gets all the Properties of a Guild as a {@link Properties} object
+	 * @param g the {@link Guild}
+	 * @return the Properties
+	 */
 	private static Properties getGuildSpecificProperties(Guild g) {
 		if (!guildProps.containsKey(g)) {
 			guildProps.put(g,loadGuildSpecificProperties(g));
 		}
 		return guildProps.get(g);
 	}
+	/**
+	 * loads all the Properties of a Guild
+	 * @param g the {@link Guild}
+	 * @return the Properties
+	 */
 	private static Properties loadGuildSpecificProperties(Guild g) {
 		return loadPropertiesWithoutGenerating("Guild"+g.getId()+".properties", getDefaultProperties());
 	}
+	/**
+	 * saves all the Properties of a Guild
+	 * @param props the Properties
+	 * @param g the {@link Guild}
+	 */
 	private static void saveGuildSpecificProperties(Properties props,Guild g) {
 		saveProperties("Guild"+g.getId()+".properties", props, "Guild specific Properties for Guild "+g.getName());
 	}
+	/**
+	 * saves all the Properties of all {@link Guild}s<br>
+	 * These Properties can be overwritten for each {@link Guild}
+	 * @param props the Properties
+	 */
 	private static void saveGuildDefaultProperties(Properties props) {
 		saveProperties("Guild.properties", props, "Default Properties of Nightdream");
 	}
+	/**
+	 * loads all the Properties of all {@link Guild}s as a {@link Properties} object<br>
+	 * These Properties can be overwritten for each {@link Guild}
+	 * @return the Properties
+	 */
 	private static Properties loadGuildDefaultProperties() {
 		Map<String,String> defaults=new HashMap<>();
 		defaults.put(PREFIX_PROP_NAME, "nd-");
 		defaults.put("MsgLogChannel", "");
 		return loadProperties("Guild.properties", defaults, "Default Properties of Nightdream");
 	}
+	/**
+	 * gets all the Properties of all {@link Guild}s as a {@link Properties} object<br>
+	 * These Properties can be overwritten for each {@link Guild}
+	 * @return the Properties
+	 */
 	private static Properties getDefaultProperties() {
 		if (defaultProps==null) {
 			defaultProps=loadGuildDefaultProperties();
 		}
 		return defaultProps;
 	}
+	/**
+	 * gets a Property that is valid for the whole Bot
+	 * @param key the key of the Property
+	 * @return the value of the Property
+	 */
 	public static String getGlobalProperty(String key) {
 		return getGlobalProperties().getProperty(key);
 	}
+	/**
+	 * sets a Property that is valid for the whole Bot
+	 * @param key the key of the Property
+	 * @param value the value of the Property
+	 */
 	public static void setGlobalProperty(String key,String value) {
 		Properties props=getGlobalProperties();
 		props.setProperty(key, value);
 		saveProperties("NightDream.properties", props, "Nightdream Properties");
 	}
+	/**
+	 * gets all Properties that are valid for the whole Bot
+	 * @return the global {@link Properties}
+	 */
 	private static Properties getGlobalProperties() {
 		if(globalProps==null) {
 			Map<String,String> defaults=new HashMap<>();
@@ -122,6 +226,13 @@ public class BotData {
 		}
 		return globalProps;
 	}
+	/**
+	 * loads Properties from a file
+	 * @param filename the name of the file(or path relative to the directory to the Bot)
+	 * @param defaults the default Properties as {@link Map Map&lt;String,String&gt;}
+	 * @param comment a comment for the newly generated file if it does not exist
+	 * @return the loaded Properties
+	 */
 	public static Properties loadProperties(String filename,Map<String,String> defaults,String comment) {
 		Properties props=new Properties();
 		props.putAll(defaults);
@@ -141,6 +252,12 @@ public class BotData {
 		}
 		return props;
 	}
+	/**
+	 * loads Properties from a file
+	 * @param filename the name of the file(or path relative to the directory to the Bot)
+	 * @param defaultProperties the default Properties as {@link Properties}
+	 * @return the loaded Properties
+	 */
 	public static Properties loadPropertiesWithoutGenerating(String filename, Properties defaultProperties) {
 		Properties props=new Properties(defaultProperties);
 		File file=new File(DATA_DIR,filename);
@@ -153,6 +270,12 @@ public class BotData {
 		}
 		return props;
 	}
+	/**
+	 * saves Properties to a file
+	 * @param filename the name of the file(or path relative to the directory to the Bot)
+	 * @param props the data to be saved
+	 * @param comment a comment for the newly generated file
+	 */
 	public static void saveProperties(String filename,Properties props, String comment) {
 		try(Writer writer=new FileWriter(new File(DATA_DIR,filename))){
 			props.store(writer,comment);
@@ -160,24 +283,51 @@ public class BotData {
 			// ignore
 		}
 	}
+	/**
+	 * sets the Bug Report channel of the Bot
+	 * @param channelID the ISnowflake ID of the channel
+	 */
 	public static void setBugReportChannel(String channelID) {
 		setGlobalProperty("BugReportChannel", channelID);
 	}
+	/**
+	 * gets the Bug Report channel of the Bot
+	 * @return the ISnowflake ID of the channel
+	 */
 	public static String getBugReportChannel() {
 		return getGlobalProperty("BugReportChannel");
 	}
+	/**
+	 * sets the bug current bug ID of the Bot
+	 * @param bugID the Bug id
+	 */
 	public static void setBugID(int bugID) {
 		setGlobalProperty("BugID", String.valueOf(bugID));
 	}
+	/**
+	 * gets the bug current bug ID of the Bot
+	 * @return the Bug id
+	 */
 	public static int getBugID() {
 		return Integer.parseInt(getGlobalProperty("BugID"));
 	}
+	/**
+	 * sets the channel for fixed bugs of the bot
+	 * @param channelID the id of the channel
+	 */
 	public static void setFixedBugsChannel(String channelID) {
 		setGlobalProperty("FixedBugsChannel", channelID);
 	}
+	/**
+	 * gets the channel for fixed bugs of the bot
+	 * @return the id of the channel
+	 */
 	public static String getFixedBugsChannel() {
 		return getGlobalProperty("FixedBugsChannel");
 	}
+	/**
+	 * reloads all Properties
+	 */
 	public static void reloadAllProperties() {
 		globalProps=null;
 		getGlobalProperties();
@@ -185,7 +335,10 @@ public class BotData {
 		guildProps.clear();
 		Profile.reload();
 	}
-
+	/**
+	 * removes the Properties for a Guilld
+	 * @param guild the {@link Guild}
+	 */
 	public static void reloadGuildProperties(Guild guild) {
 		guildProps.remove(guild);
 	}
--- src/main/java/io/github/bynoobiyt/nightdream/util/GeneralUtils.java
+++ src/main/java/io/github/bynoobiyt/nightdream/util/GeneralUtils.java
@@ -2,12 +2,23 @@ package io.github.bynoobiyt.nightdream.util;
 
 import java.util.Random;
 
+/**
+ * some utilities that are not specifically for Discord
+ * @author Daniel Schmid
+ *
+ */
 public class GeneralUtils {
 	private static final Random rand=new Random();
 	
 	private GeneralUtils(){
 		//prevent instantiation
 	}
+	/**
+	 * gets the string value of the first occurence of a key in a json String
+	 * @param json the JSON String
+	 * @param query the key to look for
+	 * @return the value of the first occurence
+	 */
 	public static String getJSONString(String json,String query) {
 		String str="\""+query+"\":\"";
 		if(json.indexOf(str)<0) {
@@ -16,6 +27,12 @@ public class GeneralUtils {
 		int startIndex=json.indexOf(str)+str.length();
 		return json.substring(startIndex,json.indexOf('\"', startIndex));
 	}
+	/**
+	 * gets the string array value of the first occurence of a key in a json String
+	 * @param json the JSON String
+	 * @param query the key to look for
+	 * @return the value of the first occurence
+	 */
 	public static String getMultipleJSONStrings(String json,String query) {
 		String str="\""+query+"\":[\"";
 		if(json.indexOf(str)<0) {
@@ -24,9 +41,20 @@ public class GeneralUtils {
 		int startIndex=json.indexOf(str)+str.length();
 		return json.substring(startIndex,json.indexOf("\"]", startIndex)).replace("\"", "").replace(",", ", ");
 	}
+	/**
+	 * calculates a random integer
+	 * @return the random integer
+	 */
 	public static int getRandInt() {
 		return rand.nextInt();
 	}
+	/**
+	 * calculates a random integer<br>
+	 * from and to can be swapped.
+	 * @param from the min value(inclusive) of the int
+	 * @param to the max value(exclusive) of the int
+	 * @return the random integer
+	 */
 	public static int getRandInt(int from, int to) {
 		if(from>to) {
 			from^=to;
@@ -35,9 +63,18 @@ public class GeneralUtils {
 		}
 		return rand.nextInt(to-from)+from;
 	}
+	/**
+	 * calculates a random integer
+	 * @param bound the max value(exclusive) of the int
+	 * @return the random integer
+	 */
 	public static int getRandInt(int bound) {
 		return rand.nextInt(bound);
 	}
+	/**
+	 * gets the {@link Random} object used for randomness calculation
+	 * @return the {@link Random} object
+	 */
 	public static Random getRand() {
 		return rand;
 	}
