e9d618ef8a5c0c87a9faed8e1ffd1a7727d2947b
danthe1st <daniel@wwwmaster.at>
2019-10-17 16:19:18 +0200

    mvn test in CI

--- src/test/java/io/github/jdiscordbots/nightdream/JDALoader.java
+++ src/test/java/io/github/jdiscordbots/nightdream/JDALoader.java
@@ -1,10 +1,21 @@
 package io.github.jdiscordbots.nightdream;
 
 import io.github.jdiscordbots.nightdream.core.NightDream;
+import io.github.jdiscordbots.nightdream.util.BotData;
 import net.dv8tion.jda.api.JDA;
 
 public class JDALoader {
+	private static void copyEnv(String... keys) {
+		for (String key : keys) {
+			String value=System.getenv("ND_"+key);
+			if(value!=null) {
+				System.setProperty(key, value);
+			}
+		}
+	}
 	public static JDA load() {
+		copyEnv("token","admin");
+		BotData.setGame("Unit-Testing");
 		System.setProperty("profile", "test");
 		return NightDream.initialize();
 	}
