35f6715d2798b137fe095bf4a89359aafeae9eae
danthe1st <daniel@wwwmaster.at>
2019-11-06 14:57:16 +0100

    sonar refactoring, update pom

--- src/main/java/io/github/jdiscordbots/nightdream/commands/Shell.java
+++ src/main/java/io/github/jdiscordbots/nightdream/commands/Shell.java
@@ -16,12 +16,20 @@ import io.github.jdiscordbots.nightdream.logging.LogType;
 import io.github.jdiscordbots.nightdream.logging.NDLogger;
 import io.github.jdiscordbots.nightdream.util.JDAUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 @BotCommand("shell")
 public class Shell implements Command {
 
-	private ExecutorService threadPool=Executors.newCachedThreadPool();
+	private static final String FIELD_START="```bash\n";
+	private static final String FIELD_END="```\n\n";
+	
+	private ExecutorService threadPool=Executors.newCachedThreadPool(r->{
+        Thread t = Executors.defaultThreadFactory().newThread(r);
+        t.setDaemon(true);
+        return t;
+    });
 	private Timer timer=new Timer(true);
 	
 	@Override
@@ -32,6 +40,25 @@ public class Shell implements Command {
 	public String permNeeded() {
 		return "Bot-Admin";
 	}
+	private void startAutoKill(Process p,TextChannel chan) {
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				if(p.isAlive()) {
+					chan.sendMessage("Excuse the wait, the command is still executing! The process will be killed in 10 more seconds.").queue();
+					timer.schedule(new TimerTask() {
+						@Override
+						public void run() {
+							if(p.isAlive()) {
+								p.destroy();
+								chan.sendMessage("Process was killed after being unresponsive for 20 seconds.").queue();
+							}
+						}
+					}, 10*1000L);
+				}
+			}
+		}, 10*1000L);
+	}
 	@Override
 	public void action(String[] args, GuildMessageReceivedEvent event) {
 		if(args.length<1) {
@@ -39,27 +66,11 @@ public class Shell implements Command {
 			return;
 		}
 		EmbedBuilder eb=new EmbedBuilder();
-		eb.setDescription("**Command**: ```bash\n"+String.join(" ", args)+"```\n\n");
+		eb.setDescription("**Command**: ```bash\n"+String.join(" ", args)+FIELD_END);
 		ProcessBuilder builder=new ProcessBuilder(args);
 		try {
 			Process p=builder.start();
-			timer.schedule(new TimerTask() {
-				@Override
-				public void run() {
-					if(p.isAlive()) {
-						event.getChannel().sendMessage("Excuse the wait, the command is still executing! The process will be killed in 10 more seconds.").queue();
-						timer.schedule(new TimerTask() {
-							@Override
-							public void run() {
-								if(p.isAlive()) {
-									p.destroy();
-									event.getChannel().sendMessage("Process was killed after being unresponsive for 20 seconds.").queue();
-								}
-							}
-						}, 10*1000);
-					}
-				}
-			}, 10*1000);
+			startAutoKill(p,event.getChannel());
 			threadPool.execute(()->{
                 try(BufferedReader stdout=new BufferedReader(new InputStreamReader(p.getInputStream(),Charset.defaultCharset()));
             		BufferedReader stderr=new BufferedReader(new InputStreamReader(p.getErrorStream(),Charset.defaultCharset()))){
@@ -69,10 +80,10 @@ public class Shell implements Command {
 	                String out=stdout.lines().collect(Collectors.joining("\n"));
 	                String err=stderr.lines().collect(Collectors.joining("\n"));
 	                if(!"".equals(out)) {
-	                	eb.appendDescription("**Output**(`stdout`): ```bash\n"+out+"```\n\n");
+	                	eb.appendDescription("**Output**(`stdout`): "+FIELD_START+out+FIELD_END);
 	                }
 	                if(!"".equals(err)) {
-	                	eb.appendDescription("**Errors**(`stderr`): ```bash\n"+err+"```\n\n");
+	                	eb.appendDescription("**Errors**(`stderr`): "+FIELD_START+err+FIELD_END);
 	                }
 	            }catch(InterruptedException e) {
 					Thread.currentThread().interrupt();
