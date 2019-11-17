ec69367e7f2d9d09738ff77d4c4536f95001e4f5
danthe1st <daniel@wwwmaster.at>
2019-11-17 14:06:23 +0100

    deleted old travis deploy script

--- saveJAR.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/byNoobiYT/NightDream.wiki.git wiki
-mvn package -DskipTests=true
-cp target/NightDream.jar wiki/NightDream.jar
-cd wiki
-git add NightDream.jar
-git commit -m "CI JAR deploy: `date`"
-git push https://$githubUsername:$githubToken@github.com/JDiscordBots/NightDream.wiki.git
