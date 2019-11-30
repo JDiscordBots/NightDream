f6b6c95ead1231c5ee6605d3364e10cae2e8d448
danthe1st <daniel@wwwmaster.at>
2019-11-30 15:42:32 +0100

    fixed eval tests

--- src/test/java/io/github/jdiscordbots/nightdream/commands/EvalTest.java
+++ src/test/java/io/github/jdiscordbots/nightdream/commands/EvalTest.java
@@ -1,16 +1,19 @@
 package io.github.jdiscordbots.nightdream.commands;
 
 import static io.github.jdiscordbots.jdatesting.TestUtils.getMessage;
+import static io.github.jdiscordbots.jdatesting.TestUtils.hasEmbed;
 import static io.github.jdiscordbots.jdatesting.TestUtils.sendCommand;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import io.github.jdiscordbots.nightdream.commands.Command.CommandType;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.internal.entities.ReceivedMessage;
 
 public class EvalTest extends AbstractAdminCommandTest{
 	@Test 
@@ -34,15 +37,17 @@ public class EvalTest extends AbstractAdminCommandTest{
 	@Test
 	public void testSimpleExpression() {
 		sendCommand("eval 1+1");
-		Message resp=getMessage("```java\n2\n```");
+		Message resp=getMessage(msg->hasEmbed(msg,null, "```java\n2\n```"));
 		assertNotNull(resp);
+		assertTrue(hasEmbed(resp, embed->embed.getFooter().getText().startsWith("java.lang.Integer | ")));
 		resp.delete().queue();
 	}
 	@Test
 	public void testCorrectMessageObj() {
 		sendCommand("eval message");
-		Message resp=getMessage("```java\n"+getMessage(msg->msg.getContentRaw().endsWith("eval message"))+"\n```");
+		Message resp=getMessage(msg->hasEmbed(msg,null, "```java\n"+getMessage(message->message.getContentRaw().endsWith("eval message"))+"\n```"));
 		assertNotNull(resp);
+		assertTrue(hasEmbed(resp, embed->embed.getFooter().getText().startsWith(ReceivedMessage.class.getCanonicalName()+" | ")));
 		resp.delete().queue();
 	}
 	@Override
