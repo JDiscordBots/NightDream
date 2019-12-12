4c33a3ac5f61ed9b763e9003bd59084dfe1dd36c
danthe1st <daniel@wwwmaster.at>
2019-12-12 16:27:20 +0100

    info test

---
+++ src/test/java/io/github/jdiscordbots/nightdream/commands/InfoTest.java
@@ -0,0 +1,36 @@
+package io.github.jdiscordbots.nightdream.commands;
+
+import static io.github.jdiscordbots.jdatesting.TestUtils.getJDA;
+import static io.github.jdiscordbots.jdatesting.TestUtils.getMessage;
+import static io.github.jdiscordbots.jdatesting.TestUtils.sendCommand;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.jdiscordbots.nightdream.commands.Command.CommandType;
+import net.dv8tion.jda.api.entities.Message;
+
+public class InfoTest {
+	
+	@Test
+	public void testInfo() {
+		sendCommand("info");
+		Message resp=getMessage(msg->msg.getMentionedUsers().size()==1&&
+				msg.getMentionedUsers().get(0).equals(getJDA().getSelfUser())&&
+				msg.getAttachments().size()==1&&
+				msg.getAttachments().get(0).isImage());
+		assertNotNull(resp);
+		resp.delete().queue();
+	}
+
+	@Test
+	public void testHelp() {
+		assertEquals("Displays bot information", new Info().help());
+	}
+	@Test
+	public void testCommandType() {
+		assertSame(CommandType.META, new Info().getType());
+	}
+}
