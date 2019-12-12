9e0fbeb7df8e40ed5ad9316ebd106f3503535e0d
danthe1st <daniel@wwwmaster.at>
2019-12-12 16:01:58 +0100

    footer check in help test

--- src/test/java/io/github/jdiscordbots/nightdream/commands/HelpTest.java
+++ src/test/java/io/github/jdiscordbots/nightdream/commands/HelpTest.java
@@ -1,6 +1,7 @@
 package io.github.jdiscordbots.nightdream.commands;
 
 import static io.github.jdiscordbots.jdatesting.TestUtils.getMessage;
+import static io.github.jdiscordbots.jdatesting.TestUtils.getTestingChannel;
 import static io.github.jdiscordbots.jdatesting.TestUtils.hasEmbed;
 import static io.github.jdiscordbots.jdatesting.TestUtils.hasEmbedField;
 import static io.github.jdiscordbots.jdatesting.TestUtils.sendCommand;
@@ -12,17 +13,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import io.github.jdiscordbots.nightdream.commands.Command.CommandType;
+import io.github.jdiscordbots.nightdream.core.NightDream;
+import io.github.jdiscordbots.nightdream.util.BotData;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.MessageEmbed.Field;
 
 public class HelpTest {
+	private Guild g=getTestingChannel().getGuild();
 
 	@Test
 	public void testWithoutArguments() {
 		sendCommand("help");
 		Message resp=getMessage(msg->hasEmbed(msg, embed->"Nightdream Commands".equals(embed.getTitle())));
 		assertNotNull(resp);
+		assertTrue(hasEmbed(resp, embed->("Prefix in "+g.getName()+": "+BotData.getPrefix(g)+" | Release "+NightDream.VERSION).equals(embed.getFooter().getText())));
 		assertTrue(hasEmbedField(resp, field->"Util".equals(field.getName())));
 		assertTrue(hasEmbedField(resp, field->"Fun".equals(field.getName())));
 		assertTrue(hasEmbedField(resp, field->"Config".equals(field.getName())));
@@ -41,6 +47,7 @@ public class HelpTest {
 		sendCommand("help help");
 		Message resp=getMessage(msg->hasEmbed(msg, embed->"Help with help".equals(embed.getTitle())));
 		assertNotNull(resp);
+		assertTrue(hasEmbed(resp, embed->("Prefix in "+g.getName()+": "+BotData.getPrefix(g)+" | Release "+NightDream.VERSION).equals(embed.getFooter().getText())));
 		assertTrue(hasEmbedField(resp, "Category","Meta"));
 		assertTrue(hasEmbedField(resp, "Permissions needed","<none>"));
 		assertTrue(hasEmbedField(resp, "Description","¯\\_(ツ)_/¯"));
@@ -51,6 +58,7 @@ public class HelpTest {
 		sendCommand("help eval");
 		Message resp=getMessage(msg->hasEmbed(msg, embed->"Help with eval".equals(embed.getTitle())));
 		assertNotNull(resp);
+		assertTrue(hasEmbed(resp, embed->("Prefix in "+g.getName()+": "+BotData.getPrefix(g)+" | Release "+NightDream.VERSION).equals(embed.getFooter().getText())));
 		assertTrue(hasEmbedField(resp, "Category","Meta"));
 		assertTrue(hasEmbedField(resp, "Permissions needed","Bot-Admin"));
 		assertTrue(hasEmbedField(resp, "Description","Evaluates Code"));
