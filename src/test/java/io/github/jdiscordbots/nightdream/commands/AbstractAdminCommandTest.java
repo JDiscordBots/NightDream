5385a7e6b8a03939a58a6a6ac0a005193fd1d7e7
danthe1st <daniel@wwwmaster.at>
2019-12-20 17:12:30 +0100

    more admin test logs

--- src/test/java/io/github/jdiscordbots/nightdream/commands/AbstractAdminCommandTest.java
+++ src/test/java/io/github/jdiscordbots/nightdream/commands/AbstractAdminCommandTest.java
@@ -6,12 +6,15 @@ import static io.github.jdiscordbots.jdatesting.TestUtils.sendCommand;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import io.github.jdiscordbots.jdatesting.TestUtils;
+import io.github.jdiscordbots.nightdream.logging.LogType;
+import io.github.jdiscordbots.nightdream.logging.NDLogger;
 import io.github.jdiscordbots.nightdream.util.BotData;
 import net.dv8tion.jda.api.entities.Message;
 
@@ -26,13 +29,16 @@ public abstract class AbstractAdminCommandTest {
 	}
 	@Test
 	public void testNonAdmin() {
+		NDLogger.logWithModule(LogType.DEBUG,"test","Admins before non-admin test: "+Arrays.toString(BotData.getAdminIDs()));
 		String[] adminIDs=BotData.getAdminIDs();
 		BotData.setAdminIDs(Stream.of(adminIDs).filter(id->!id.equals(getJDA().getSelfUser().getId())).toArray(String[]::new));
 		sendCommand(cmdName());
 		Message resp=getMessage(msg->msg.getContentRaw().endsWith(" This is an admin command."));
+		NDLogger.logWithModule(LogType.DEBUG,"test","Admins during non-admin test: "+Arrays.toString(BotData.getAdminIDs()));
 		assertNotNull(resp);
 		resp.delete().queue();
 		BotData.setAdminIDs(adminIDs);
+		NDLogger.logWithModule(LogType.DEBUG,"test","reset admins: "+Arrays.toString(BotData.getAdminIDs()));
 	}
 	protected abstract String cmdName();
 	protected abstract Command cmd();
