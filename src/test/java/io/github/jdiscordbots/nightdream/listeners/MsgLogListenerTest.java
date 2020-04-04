67afa05f1e2ca13f5c4c8c20bea17c387f697725
danthe1st <daniel@wwwmaster.at>
2020-04-04 19:06:29 +0200

    different msg titles in msglog listener tests

--- src/test/java/io/github/jdiscordbots/nightdream/listeners/MsgLogListenerTest.java
+++ src/test/java/io/github/jdiscordbots/nightdream/listeners/MsgLogListenerTest.java
@@ -33,20 +33,20 @@ public class MsgLogListenerTest {
 	
 	@Test
 	public void testNotCachedMsg() {
-		Message toDelete = getTestingChannel().sendMessage("test message").complete();
+		Message toDelete = getTestingChannel().sendMessage("test message 1").complete();
 		listener.clearCache();
 		toDelete.delete().complete();
-		assertNull(getAlreadySentMessage(getTestingChannel(), msg->hasEmbed(msg, "Deleted Message",null)));
+		assertNull(getAlreadySentMessage(getTestingChannel(), msg->hasEmbed(msg, "Deleted Message 1",null)));
 	}
 	@Test
 	public void testDeletedMsg() {
-		Message toDelete=getTestingChannel().sendMessage("test message")
+		Message toDelete=getTestingChannel().sendMessage("test message 2")
 		.addFile(new byte[10], "empty.txt")
 		.complete();
 		toDelete.delete().queue();
 		Message resp=getMessage(msg->hasEmbed(msg, "Deleted Message",null));
 		assertNotNull(resp);
-		assertTrue(hasEmbedField(resp, "Message","test message"));
+		assertTrue(hasEmbedField(resp, "Message","test message 2"));
 		String avatarURL=getJDA().getSelfUser().getAvatarUrl();
 		assertTrue(hasEmbed(resp, embed->avatarURL==null?embed.getThumbnail()==null:avatarURL.equals(embed.getThumbnail().getUrl())));
 		assertTrue(hasEmbedField(resp, "Attachments","[empty.txt]("+toDelete.getAttachments().get(0).getUrl()+") (10)"));
