b5a38610a48c87784866cc8b1c789455641843e6
danthe1st <daniel@wwwmaster.at>
2021-06-11 11:16:19 +0200

    regenerate notice, fix timing in seval tests

--- src/test/java/io/github/jdiscordbots/nightdream/commands/SEvalTest.java
+++ src/test/java/io/github/jdiscordbots/nightdream/commands/SEvalTest.java
@@ -10,13 +10,17 @@ package io.github.jdiscordbots.nightdream.commands;
 import static io.github.jdiscordbots.jdatesting.TestUtils.getAlreadySentMessage;
 import static io.github.jdiscordbots.jdatesting.TestUtils.getMessage;
 import static io.github.jdiscordbots.jdatesting.TestUtils.getTestingChannel;
+import static io.github.jdiscordbots.jdatesting.TestUtils.getTimeout;
 import static io.github.jdiscordbots.jdatesting.TestUtils.hasEmbed;
 import static io.github.jdiscordbots.jdatesting.TestUtils.sendCommand;
+import static io.github.jdiscordbots.jdatesting.TestUtils.setTimeout;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.awaitility.Awaitility;
+import org.awaitility.Durations;
 import org.junit.jupiter.api.Test;
 
 import io.github.jdiscordbots.jdatesting.TestUtils;
@@ -27,18 +31,26 @@ public class SEvalTest extends AbstractAdminCommandTest{
 	
 	@Test 
 	public void testSuccessfulExecution() {
-		sendCommand("seval channel.sendMessage(\"Hello\").queue();");
-		Message resp=getMessage("Hello");
-		assertNotNull(resp);
-		resp.delete().queue();
-		resp=getAlreadySentMessage(getTestingChannel(),msg->hasEmbed(msg, embed->embed.getFooter()!=null&&embed.getFooter().getText().startsWith("null | ")));
-		assertNull(resp);
+		Duration prevTimeout = getTimeout();
+		try {
+			setTimeout(prevTimeout.multipliedBy(3));
+			sendCommand("seval channel.sendMessage(\"Hello\").queue();");
+			Message resp = getMessage("Hello");
+			assertNotNull(resp);
+			resp.delete().queue();
+			resp = getAlreadySentMessage(getTestingChannel(), msg -> hasEmbed(msg,
+					embed -> embed.getFooter() != null && embed.getFooter().getText().startsWith("null | ")));
+			assertNull(resp);
+		} finally {
+			TestUtils.setTimeout(prevTimeout);
+		}
 	}
 	@Test 
 	public void testError() {
 		int msgsToChack=TestUtils.getNumOfMessagesToCheck();
 		try {
 			TestUtils.setNumOfMessagesToCheck(5);
+			long timeBefore=System.currentTimeMillis();
 			sendCommand("seval thisisinvalid");
 			sendCommand("seval thisisinvalidtoo");
 			Message resp=getMessage("No...");
@@ -47,7 +59,9 @@ public class SEvalTest extends AbstractAdminCommandTest{
 			assertNotNull(respDel);
 			assertNotEquals(resp.getId(),respDel.getId());
 			resp.clearReactions().complete();
-			Awaitility.await().atLeast(59, TimeUnit.SECONDS).atMost(65,TimeUnit.SECONDS).until(()->getAlreadySentMessage(getTestingChannel(),msg->msg.getId().equals(respDel.getId()))==null);
+			long timeAfter=System.currentTimeMillis();
+			int timeDiffSeconds=(int) Math.ceil((timeAfter-timeBefore)/(double)100);
+			Awaitility.await().atLeast(60-timeDiffSeconds, TimeUnit.SECONDS).atMost(65,TimeUnit.SECONDS).until(()->getAlreadySentMessage(getTestingChannel(),msg->msg.getId().equals(respDel.getId()))==null);
 			Message stillResp=getAlreadySentMessage(getTestingChannel(), msg->msg.getIdLong()==resp.getIdLong());
 			assertNotNull(stillResp);
 			stillResp.delete().queue();
